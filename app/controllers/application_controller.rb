c4d4b50044666fa1366c7750596e1fecaacbf114
KodeBro <brorcs@gmail.com>
2017-07-28 08:29:48 +0000

    nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:about_me, :display_name,
+                                                                :password,
+                                                                :password_confrimation])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
+  devise_for :users
   root 'pages#home'
 
-  get 'contact' => 'pages#contact'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
 end
